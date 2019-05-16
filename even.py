823bdb828fcab3eb2bfb16907e32b5a6f2d06606
srikanthpragada <srikanthpragada@gmail.com>
2019-05-16 09:58:57 +0530

    16-MAY

---
+++ even.py
@@ -0,0 +1,8 @@
+# Even number
+
+num = int(input("Enter a number :"))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
---
+++ for_demo.py
@@ -0,0 +1,9 @@
+
+for i in range(5):
+    if i > 4:
+        break
+    print(i)
+else:
+    print("Done")
+
+print("All Over")
