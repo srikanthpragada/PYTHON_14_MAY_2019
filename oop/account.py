149eb91a7762ea7740d55e67396f978008470c58
srikanthpragada <srikanthpragada@gmail.com>
2019-05-29 09:28:23 +0530

    29-MAY

--- oop/account.py
+++ oop/account.py
@@ -15,14 +15,33 @@ class Account:
     def get_balance(self):
         return self.balance
 
-    def get_minbal(self):
-        return self.minbal
+    @staticmethod
+    def get_minbal():
+        return Account.minbal
 
+    def __str__(self):
+        return f"{self.acno} - {self.customer} - {self.balance}"
 
-a1 = Account(1, "Scott")  # Object
-a2 = Account(2, "Tim", 10000)
+    def __eq__(self, other):
+        return self.acno == other.acno
 
-a1.deposit(50000)
-a1.deposit(10000)
-print(a1.get_balance())
-print(a1.get_minbal())
+    def __gt__(self, other):
+        return self.balance > other.balance
+
+    @property
+    def current_balance(self):
+        return self.balance
+
+if __name__ == '__main__':
+    a1 = Account(1, "Scott", 15000)  # Object
+    print(a1.current_balance)  #property
+    a2 = Account(2, "Steve", 10000)
+
+    print(a1 == a2)  # a1.__eq__(a2)
+    print(a1 > a2)
+
+    print(str(a1))  # a1.__str__()
+
+    a1.deposit(50000)
+    print(a1.get_balance())
+    print(Account.get_minbal())
